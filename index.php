4a670c224f52005f69d36dec7c17fbf250ba42dd
bagoes48 <bagus.manto1@gmail.com>
2015-12-14 23:32:38 +0800

    Penambahan background dan juga warna merah material

---
+++ index.php
@@ -0,0 +1,420 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dokumen</title>
+	<link rel="stylesheet" href="css/ionicons.min.css">
+	<style type="text/css">
+
+	::selection {
+		color:#fff;
+        background: firebrick;
+	}
+
+	body {
+		margin:0px;
+		padding: 0px;
+        font-family:'raleway';
+	}
+	header {
+
+		
+
+
+		background:linear-gradient(90deg, rgba(6, 146, 240, 0.2) 13%, rgba(0,0,0, 0.8) 100%), url('img/banner.jpeg');
+		background-position:center;
+		height:600px;
+		width:100%;
+		background-attachment:fixed;
+		background-size:cover;
+	}
+
+	h1.headlines {
+		padding:100px;
+		margin:0;
+		color:#fff;
+		font-family: 'raleway';
+		font-size: 60px;
+		text-align: center;
+        font-weight: 200;
+	}
+
+	p.introduction {
+		font-size: 18px;
+		padding:100px 300px;
+		line-height: 150%;
+		font-family: 'raleway';
+		color:#555;
+		font-weight: 400;
+		text-shadow: 0px 1px 0px #333;
+	}
+        
+    p.people {
+    	text-transform: uppercase;
+    	color:#fff;
+    	text-align:center;
+    	font-size:32px;
+    	margin-top:-80px;
+    	font-family:'raleway';
+    }
+
+        
+        nav.menu {
+           width:100%;
+            text-align: center;
+            color:#333;
+            background:#fff;
+        }
+        
+        nav.menu ul {
+            padding:0px;
+            margin:0px;
+        }
+        
+        nav.menu ul li {
+            list-style: none;
+            display: inline-block;
+        }
+        
+        nav.menu ul li a {
+            display: block;
+            padding:23px;
+            color:rgba(232, 29, 98, 1);
+            font-family: 'raleway';
+            font-weight: 200;
+            font-size:22px;
+            text-decoration: none;
+        }
+        
+        nav.menu ul li a i {
+            padding: 10px;
+        }
+        
+/*
+        nav.menu ul li.active {
+            border-top: 10px solid #333;
+        }
+*/
+	</style>
+</head>
+<body>
+
+<header>
+	
+	
+    <nav class="menu">
+        <ul>
+            <li class="active">
+                <a href="#">
+                    <i class="icon ion-document-text"></i>
+                    Blog
+                </a>
+            </li>
+            
+            <li>
+                <a href="#">
+                   <i class="icon ion-code"></i>
+                    Code
+                </a>
+            </li>
+            
+            <li>
+                <a href="#">
+                   <i class="icon ion-erlenmeyer-flask"></i>
+                    Experiment
+                </a>
+            </li>
+        </ul>
+    </nav>
+    
+    
+    <h1 class="headlines">
+		Contribute to XAMPP translation at translate.apachefriends.org
+	</h1>
+	<p class="people">- By Bagus Mantonafi -</p>
+</header>
+
+<p class="introduction">
+	Apache Friends and Bitnami are cooperating to make dozens of open source applications available on XAMPP, for free. Bitnami-packaged applications include Wordpress, Drupal, Joomla! and dozens of others and can be deployed with one-click installers. Visit the Bitnami XAMPP page for details on the currently available apps. 
+</p>
+   
+   
+   
+   <style>
+    
+       div.heading-blog {
+background-image: linear-gradient(90deg, rgb(232, 29, 98) 35%, rgb(254, 86, 33) 100%);
+background-image: -moz-linear-gradient(left, rgb(232, 29, 98) 35%, rgb(254, 86, 33) 100%);
+background-image: -webkit-linear-gradient(left, rgb(232, 29, 98) 35%, rgb(254, 86, 33) 100%);
+background-image: -o-linear-gradient(left, rgb(232, 29, 98) 35%, rgb(254, 86, 33) 100%);
+background-image: -ms-linear-gradient(left, rgb(232, 29, 98) 35%, rgb(254, 86, 33) 100%);;
+           /*background:cornflowerblue;*/
+           box-sizing: border-box;
+           padding:5px;
+           box-shadow:0px 4px 10px 0px #ccc;
+       }
+
+       div.heading-blog h1 {
+       	font-size:50px;
+           color:#fff;
+           font-family:raleway;
+			font-weight:100;
+			margin-bottom: -30px;
+       }
+
+       div.heading-blog p {
+       	padding:20px 10%;
+       	font-size:24px;
+       	color:#fff;
+       }
+    </style>
+   
+   
+   <div class="main" id="main">
+       <div class="heading-blog">
+           <h1 class="text-center">
+           	<i class="icon ion-bookmark"></i>
+           	Blog
+           	<!-- <i class="icon ion-bookmark"></i> -->
+           	</h1>
+           <p class="text-center">
+           	"Seperti yang ada ketahui dan lihat ini adalah blog saya, ketika anda membuka blog ini maka anda akan melihat segala sesuatu yang saya suka. yang saya sajikan dalam bentuk sebuah tulisan guna mempermudah dokumentasi dan juga bisa digunakan sebagai pembelajaran"</p>
+       </div>
+        <ul class="list">
+            <li>
+               <div class="wrapper-excerpt">
+                <h2>As a young professional, you’ve probably filled your Google reader with career-related, political and news-heavy feeds.</h2>
+                
+                
+                <h4 class="meta-post">
+                By <a href="">@Bagus Mantonafi</a> | December 12, 2015 | NodeJS
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate explicabo, molestias veritatis laborum quia rerum excepturi nisi qui id, quas consequuntur. Atque, placeat velit a aperiam optio voluptates illum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus in architecto officiis fugit magni officia minus nostrum impedit, ipsam pariatur dignissimos rerum cumque, totam, ex nulla quisquam, nihil odit.</p>
+                
+                
+                <a href="" class="button">
+                Read More
+                <i class="icon ion-android-arrow-forward"></i>
+                </a>
+                </div>
+            </li>
+            
+             <li>
+               <div class="wrapper-excerpt">
+                <h2>Now working on her M.A. in communications, Renee describes herself as a political junkie, social media nerd, suburbanite and beer connoisseur.</h2>
+                
+                
+                <h4 class="meta-post">
+                By <a href="">@Bagus Mantonafi</a> | December 12, 2015 | NodeJS
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate explicabo, molestias veritatis laborum quia rerum excepturi nisi qui id, quas consequuntur. Atque, placeat velit a aperiam optio voluptates illum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus in architecto officiis fugit magni officia minus nostrum impedit, ipsam pariatur dignissimos rerum cumque, totam, ex nulla quisquam, nihil odit.</p>
+                
+                <a href="" class="button">Read More</a>
+                </div>
+               
+        
+            </li>
+            
+                         <li>
+               <div class="wrapper-excerpt">
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+                
+                
+                <h4 class="meta-post">
+                By <a href="">@Bagus Mantonafi</a> | December 12, 2015 | NodeJS
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate explicabo, molestias veritatis laborum quia rerum excepturi nisi qui id, quas consequuntur. Atque, placeat velit a aperiam optio voluptates illum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus in architecto officiis fugit magni officia minus nostrum impedit, ipsam pariatur dignissimos rerum cumque, totam, ex nulla quisquam, nihil odit.</p>
+                
+                <a href="" class="button">Read More</a>
+                </div>
+               
+        
+            </li>
+
+           
+           
+                        <li>
+               <div class="wrapper-excerpt">
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+                
+                
+                <h4 class="meta-post">
+                By <a href="">@Bagus Mantonafi</a> | December 12, 2015 | NodeJS
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate explicabo, molestias veritatis laborum quia rerum excepturi nisi qui id, quas consequuntur. Atque, placeat velit a aperiam optio voluptates illum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus in architecto officiis fugit magni officia minus nostrum impedit, ipsam pariatur dignissimos rerum cumque, totam, ex nulla quisquam, nihil odit.</p>
+                
+                <a href="" class="button">Read More</a>
+                </div>
+               
+        
+            </li>
+
+           
+           
+                        <li>
+               <div class="wrapper-excerpt">
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+                
+                
+                <h4 class="meta-post">
+                By <a href="">@Bagus Mantonafi</a> | December 12, 2015 | NodeJS
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate explicabo, molestias veritatis laborum quia rerum excepturi nisi qui id, quas consequuntur. Atque, placeat velit a aperiam optio voluptates illum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus in architecto officiis fugit magni officia minus nostrum impedit, ipsam pariatur dignissimos rerum cumque, totam, ex nulla quisquam, nihil odit.</p>
+                
+                <a href="" class="button">Read More</a>
+                </div>
+               
+        
+            </li>
+
+           
+           
+                        <li>
+               <div class="wrapper-excerpt">
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+                
+                
+                <h4 class="meta-post">
+                By <a href="">@Bagus Mantonafi</a> | December 12, 2015 | NodeJS
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto voluptate explicabo, molestias veritatis laborum quia rerum excepturi nisi qui id, quas consequuntur. Atque, placeat velit a aperiam optio voluptates illum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores necessitatibus in architecto officiis fugit magni officia minus nostrum impedit, ipsam pariatur dignissimos rerum cumque, totam, ex nulla quisquam, nihil odit.</p>
+                
+                <a href="" class="button">Read More</a>
+                </div>
+               
+        
+            </li>
+
+            <hr class="clearfix">
+        </ul>
+   </div>
+    
+    
+    
+    <style>
+        .text-center {
+            text-align: center;
+        }
+        
+        h4.meta-post {
+            padding:10px;
+        }
+        div.main {
+            background:#f8f8f8;
+        }
+        
+        div.main ul {
+            width:100%;
+            box-sizing: border-box;
+            padding:0px 40px;
+            list-style: none;
+        }
+        
+        div.main ul li {
+            box-sizing: border-box;
+            display: inline-block;
+            width:70%;
+            margin-left:5%;
+            padding:50px 40px;
+            background: #f2f2f2f;
+        }
+        
+        div.main ul li div.wrapper-excerpt {
+            background: #fff;
+            box-shadow: 0px 2px 5px 0px #ccc;
+            padding:20px 40px;
+        }
+        
+        div.main ul li div.wrapper-excerpt:hover {
+            box-shadow: 0px 2px 10px 0px #ccc;
+        }
+        
+        
+        
+        div.main ul li h2 {
+            color:#333;
+            font-size:32px;
+            line-height: 150%;
+        }
+        
+        div.main ul li h4 {
+            color:#aaa;
+            font-weight: 600;
+        }
+        
+        div.main ul li h4 a {
+            color:darkorange;
+            text-decoration: none;
+        }
+        div.main ul li p {
+            color:#555;
+            line-height: 150%;
+            font-size: 18px;
+        }
+        
+        
+        
+        
+/*
+        div.main ul li div.image-excerpt {
+            float:left;
+            width:50%;
+            background: #fff;
+            box-sizing: border-box;
+            box-shadow: 0px 0px 5px 0px #ccc;
+        }
+*/
+        
+        hr.clearfix {
+            clear: both;
+            opacity:0;
+        }
+        
+        
+        a.button {
+            width:80%;
+            text-align: center;
+            border:3px solid firebrick;
+            display: block;
+            padding: 20px;
+            box-sizing: border-box;
+            border-radius: 20px;
+            color:firebrick;
+            text-decoration: none;
+            font-size: 22px;
+            font-weight: 700;
+            margin:30px auto;
+        }
+    </style>
+
+
+<div id="box">0</div>
+
+
+    <style>
+        #box {
+            padding:10px;
+            top:10px;
+            font-size: 30px;
+            right:10px;
+            background: #333;
+            color:#fff;
+            position: fixed;
+        }
+    </style>
+
+
+	<script type="text/javascript">
+        var scroll = function(){
+            
+            var boxelem = document.getElementById("box");
+            var main = document.getElementById("main");
+            var y = main.scrollTop;
+            
+            boxelem.innerHTML = y;
+        }
+        
+        
+        window.onscroll = function() {scroll()};
+	</script>
+</body>
+</html>
